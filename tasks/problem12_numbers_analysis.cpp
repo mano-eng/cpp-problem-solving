86c1ec0d5f37de705b141b76e58bdf3291d32c6a
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-03-06 17:39:24 +0200

    Add files via upload

---
+++ tasks/problem12_numbers_analysis.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, ct1 = 0, ct2 = 0, sum = 0;
+
+    for(int i = 0; i < 3; i++)
+    {
+        cin >> N;
+
+        if(N % 2 != 0)
+        {
+            cout << N << endl;
+        }
+        else
+        {
+            sum += N;
+            ct1++;
+        }
+
+        if(N % 5 == 0)
+        {
+            ct2++;
+        }
+    }
+
+    if(ct1 != 0)
+    {
+        cout << "Average of even numbers = " << sum / ct1 << endl;
+    }
+
+    cout << "Numbers divisible by 5 = " << ct2 << endl;
+
+    return 0;
+}
