12afb9cddd738719af35792c945f946e91219210
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-03-07 05:55:13 +0200

    Refactor tree pods analysis program
    This program reads information about several trees and their pods, calculates the average beans weight for pods with more than 50 beans, and identifies the tree of type B with the lowest number of pods.

--- tasks/problem07_tree_pods_analysis.cpp
+++ tasks/problem07_tree_pods_analysis.cpp
@@ -1,23 +1,33 @@
+/*
+Program: Tree Pods Analysis
+
+Description:
+This program reads information about several trees and their pods.
+For each tree, it calculates the average beans weight for pods that
+contain more than 50 beans. It also determines the tree of type B
+that has the lowest number of pods.
+*/
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int N;                 // عدد الأشجار
+    int N;                 // number of trees
     cout << "Enter number of trees: ";
     cin >> N;
 
-    char T;                // نوع الشجرة (A / B / C)
-    int P;                 // عدد الـ pods في الشجرة
+    char T;                // tree type (A / B / C)
+    int P;                 // number of pods
 
-    int w;                 // وزن الـ pod
-    int wb;                // وزن الحبوب داخل الـ pod
-    int nb;                // عدد الحبوب داخل الـ pod
+    int w;                 // pod weight
+    int wb;                // beans weight
+    int nb;                // number of beans
 
-    int minPods = 99999;   // لتحديد أقل عدد pods للشجرة من النوع B
-    int treeIndex = -1;    // لتخزين رقم الشجرة الأقل
+    int minPods = 99999;   // track lowest pods for type B
+    int treeIndex = -1;
 
-    // loop تمر على كل الأشجار
+    // process all trees
     for (int i = 0; i < N; i++)
     {
         cout << "Enter type of tree (A/B/C): ";
@@ -26,10 +36,10 @@ int main()
         cout << "Enter number of pods: ";
         cin >> P;
 
-        int sum = 0;       // مجموع وزن الحبوب
-        int count = 0;     // عدد الـ pods التي عدد الحبوب فيها > 50
+        int sum = 0;
+        int count = 0;
 
-        // loop تمر على كل pods في الشجرة
+        // process pods of the tree
         for (int j = 0; j < P; j++)
         {
             cout << "Enter pod weight: ";
@@ -41,34 +51,29 @@ int main()
             cout << "Enter number of beans: ";
             cin >> nb;
 
-            // نحسب فقط pods التي عدد الحبوب فيها أكبر من 50
+            // consider pods with more than 50 beans
             if (nb > 50)
             {
-                sum += wb;   // نجمع وزن الحبوب
-                count++;     // نزيد العداد
+                sum += wb;
+                count++;
             }
         }
 
-        // حساب المتوسط لكل شجرة
         double avg = 0;
-
         if (count > 0)
             avg = (double)sum / count;
 
         cout << "Average beans weight for tree " << i + 1 << " = " << avg << endl;
 
-        // تحديد أقل عدد pods للشجرة من النوع B
-        if (T == 'B')
+        // update tree of type B with lowest pods
+        if (T == 'B' && P < minPods)
         {
-            if (P < minPods)
-            {
-                minPods = P;
-                treeIndex = i + 1;   // حفظ رقم الشجرة
-            }
+            minPods = P;
+            treeIndex = i + 1;
         }
     }
 
-    // عرض الشجرة من النوع B التي لديها أقل عدد pods
+    // final result
     cout << "Tree with lowest number of pods from type B = " << treeIndex << endl;
 
     return 0;
