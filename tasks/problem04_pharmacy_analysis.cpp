c4d3b4de02a8560c156f08d2d4066c78d555acf7
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-03-07 05:53:36 +0200

    Refactor vitamin sales analysis program
    This program analyzes vitamin sales data from a pharmacy, calculating average ages of clients who bought only Vitamin C, identifying the day with the lowest Vitamin D sales, and counting successive days with the same number of clients.

--- tasks/problem04_pharmacy_analysis.cpp
+++ tasks/problem04_pharmacy_analysis.cpp
@@ -1,3 +1,14 @@
+/*
+Program: Vitamin Sales Analysis
+
+Description:
+This program reads data for several days in a pharmacy. For each day,
+it reads the number of clients and their vitamin purchases (C and D).
+It calculates the average age of clients who bought only Vitamin C,
+finds the day with the lowest total Vitamin D sales, and counts
+successive days that have the same number of clients.
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -21,14 +32,15 @@ int main()
         int sumAgeCOnly = 0;
         int countCOnly = 0;
 
+        // process clients of the day
         for (int i = 0; i < clients; i++)
         {
             int age, vitC, vitD;
             cin >> age >> vitC >> vitD;
-       
+
             totalVitaminD += vitD;
 
-            
+            // clients who bought only Vitamin C
             if (vitC > 0 && vitD == 0)
             {
                 sumAgeCOnly += age;
@@ -36,26 +48,28 @@ int main()
             }
         }
 
-        
+        // track day with lowest Vitamin D sales
         if (d == 1 || totalVitaminD < minVitaminD)
         {
             minVitaminD = totalVitaminD;
             minDay = d;
         }
 
+        // print average age of C-only clients
         if (countCOnly > 0)
             cout << "Day " << d << " Average age (C only) = "
                  << (double)sumAgeCOnly / countCOnly << endl;
         else
             cout << "Day " << d << " Average age (C only) = 0" << endl;
 
-      
+        // count successive days with same number of clients
         if (d > 1 && clients == prevClients)
             sameDaysCount++;
 
         prevClients = clients;
     }
 
+    // final results
     cout << "Day with lowest Vitamin D sales = " << minDay << endl;
     cout << "Number of successive days with same clients = "
          << sameDaysCount << endl;
