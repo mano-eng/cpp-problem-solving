0f23b0c9a876599b50d5ea8a33d60891c1b2e384
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-04-12 17:51:26 +0200

    Refactor number splitting game with player switching

--- tasks/arrays/problem005-number-splitting-game.cpp
+++ tasks/arrays/problem005-number-splitting-game.cpp
@@ -6,56 +6,64 @@ Simulates a two-player game where a number is split into two factors.
 A player loses if the move is invalid.
 */
 
-#include <iostream> 
-using namespace std; 
- 
-int main() { 
- 
-    int arr[100]; 
- 
-    for (int i = 0; i < 100; i++) 
-        arr[i] = 0; 
- 
-    int val; 
-    cin >> val; 
- 
-    arr[0] = val; 
- 
-    int count = 0; 
- 
-    while (1) { 
- 
-        if (count % 2 == 0) 
-            cout << "Player 1\n"; 
-        else 
-            cout << "Player 2\n"; 
- 
-        int pos; 
-        cin >> pos; 
- 
-        int num1, num2; 
-        cin >> num1 >> num2; 
- 
-        if (arr[pos] <= 1 || num1 * num2 != arr[pos]) { 
-            cout << "Lose\n"; 
-            break; 
-        } 
- 
-        arr[pos] = num1; 
- 
-        for (int i = 0; i < 100; i++) { 
-            if (arr[i] == 0) { 
-                arr[i] = num2; 
-                break; 
-            } 
-        } 
- 
-        for (int i = 0; i < 100; i++) 
-            cout << arr[i] << " "; 
-        cout << endl; 
- 
-        count++; 
-    } 
- 
-    return 0; 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[100];
+
+    // تصفير
+    for (int i = 0; i < 100; i++)
+        arr[i] = 0;
+
+    int val;
+    cin >> val;
+
+    arr[0] = val;
+
+    int player = 1;
+
+    while (1) {
+
+        // طباعة اللاعب الحالي
+        cout << "Player " << player << endl;
+
+        int pos;
+        cin >> pos;
+
+        int num1, num2;
+        cin >> num1 >> num2;
+
+        // تحقق
+        if (arr[pos] <= 1 || num1 * num2 != arr[pos]) {
+            cout << "Player " << player << " lose!\n";
+            break;
+        }
+
+        // التبديل
+        arr[pos] = num1;
+
+        // الإضافة
+        for (int i = 0; i < 100; i++) {
+            if (arr[i] == 0) {
+                arr[i] = num2;
+                break;
+            }
+        }
+
+        // طباعة
+        for (int i = 0; i < 100; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+
+        // تبديل اللاعب
+        if (player == 1)
+            player = 2;
+        else
+            player = 1;
+    }
+
+    return 0;
+}
 }
