ff4112adc5b3ebd73586646e47140886c85256ea
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-04-13 18:16:38 +0200

    Refactor polynomial derivative calculation
    Refactored polynomial derivative calculation to remove user input for number of terms and use fixed size arrays.

--- tasks/arrays/problem008-polynomial-derivative.cpp
+++ tasks/arrays/problem008-polynomial-derivative.cpp
@@ -10,33 +10,26 @@ using namespace std;
 
 int main() {
     int cf[100], p[100];
-    int n;
-
-    cout << "Enter number of terms: ";
-    cin >> n;
-
-    cout << "Enter coefficients:\n";
-    for(int i = 0; i < n; i++) {
+    
+   
+    for(int i = 0; i <100 ; i++) {
         cin >> cf[i];
     }
-
-    cout << "Enter powers:\n";
-    for(int i = 0; i < n; i++) {
+  
+    for(int i = 0; i <100 ; i++) {
         cin >> p[i];
     }
-
-    for(int i = 0; i < n; i++) {
+    
+    for(int i = 0; i <100; i++) {
         cf[i] = cf[i] * p[i];
         p[i] = p[i] - 1;
     }
-
-    cout << "New Coefficients:\n";
-    for(int i = 0; i < n; i++) {
+    
+    for(int i = 0; i < 100 ; i++) {
         cout << cf[i] << " ";
     }
-
-    cout << "\nNew Powers:\n";
-    for(int i = 0; i < n; i++) {
+    
+    for(int i = 0; i < 100 ; i++) {
         cout << p[i] << " ";
     }
 
