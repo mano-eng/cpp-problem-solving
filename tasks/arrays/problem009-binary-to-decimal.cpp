bf2f4de3681428a4e48b790ad6127b467f354f1e
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-04-13 19:05:48 +0200

    Rename variable 'decimal' to 'rslt' in conversion

--- tasks/arrays/problem009-binary-to-decimal.cpp
+++ tasks/arrays/problem009-binary-to-decimal.cpp
@@ -4,13 +4,12 @@ Problem 009
 Description:
 Reads a binary number (20 bits) and converts it to decimal.
 */
-
 #include <iostream>
 using namespace std;
 
 int main() {
     int arr[20];
-    int decimal = 0, power = 1;
+    int rslt = 0, power = 1;
 
     cout << "Enter the Binary number: ";
     for (int i = 0; i < 20; i++) {
@@ -18,11 +17,11 @@ int main() {
     }
 
     for (int i = 19; i >= 0; i--) {
-        decimal += arr[i] * power;
+        rslt += arr[i] * power;
         power *= 2;
     }
 
-    cout << "The decimal number is : " << decimal;
+    cout << "The decimal number is : " << rslt;
 
     return 0;
 }
