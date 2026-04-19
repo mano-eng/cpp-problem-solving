5a52b25b5ba074a34d182ffb1c5a7beaf89c729d
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-04-19 22:48:36 +0200

    Refactor decimal to binary conversion with initialization
    Initialize the array with zeros before conversion and add comments in Arabic.

--- tasks/arrays/problem022-decimal-to-binary.cpp
+++ tasks/arrays/problem022-decimal-to-binary.cpp
@@ -8,18 +8,31 @@ Converts a decimal number into a 20-bit binary representation.
 #include <iostream>
 using namespace std;
 
-int main() {
-    int X[20] = {0};
+int main()
+{
+    int X[20];
     int n;
+
     cin >> n;
 
-    for (int i = 19; n > 0; i--) {
+    // نملّاها أصفار
+    for(int i = 0; i < 20; i++)
+    {
+        X[i] = 0;
+    }
+
+    // التحويل
+    for(int i = 19; n > 0; i--)
+    {
         X[i] = n % 2;
         n = n / 2;
     }
 
-    for (int i = 0; i < 20; i++)
+    // الطباعة
+    for(int i = 0; i < 20; i++)
+    {
         cout << X[i] << " ";
+    }
 
     return 0;
 }
