303711a3fd5df18354e754294b5477f35e1522c6
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-04-20 00:02:13 +0200

    Refactor array operations and improve user prompts

--- tasks/arrays/problem027-conditional-array-operations.cpp
+++ tasks/arrays/problem027-conditional-array-operations.cpp
@@ -5,64 +5,43 @@ Description:
 Performs different operations on array elements based on user choice.
 */
 
-#include <iostream> 
-using namespace std; 
- 
-int main() 
-{ 
-    int X[30]; 
-    int choice; 
- 
-    for(int i = 0; i < 30; i++) 
-    { 
-        cin >> X[i]; 
-    } 
- 
-    cin >> choice; 
- 
-    if(choice == 1) 
-    { 
-        for(int i = 0; i < 30; i++) 
-        { 
-            if(X[i] % 2 != 0 && X[i] > 50) 
-            { 
-                cout << X[i] << " "; 
-            } 
-        } 
-    } 
- 
-    else if(choice == 2) 
-    { 
-        int sum = 0; 
- 
-        for(int i = 0; i < 30; i++) 
-        { 
-            if(X[i] % 2 != 0) 
-            { 
-                sum += X[i]; 
-            } 
-        } 
- 
-        cout << sum; 
-    } 
- 
-    else if(choice == 3) 
-    { 
-        int sum = 0; 
-        int count = 0; 
- 
-        for(int i = 0; i < 30; i++) 
-        { 
-            if(X[i] % 2 != 0 && X[i] > 80) 
-            { 
-                sum += X[i]; 
-                count++; 
-            } 
-        } 
- 
-        if(count > 0) 
-            cout << sum / count; 
-    } 
- 
-    return 0; 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[30];
+
+    for (int i = 0; i < 30; i++)
+        cin >> arr[i];
+
+    int choice;
+    cout << "1- Odd numbers > 50\n";
+    cout << "2- Total of odd numbers\n";
+    cout << "3- Average of odd numbers > 80\n";
+    cin >> choice;
+
+    if (choice == 1) {
+        for (int i = 0; i < 30; i++)
+            if (arr[i] % 2 != 0 && arr[i] > 50)
+                cout << arr[i] << " ";
+    }
+    else if (choice == 2) {
+        int total = 0;
+        for (int i = 0; i < 30; i++)
+            if (arr[i] % 2 != 0)
+                total += arr[i];
+        cout << total;
+    }
+    else if (choice == 3) {
+        int sum = 0, count = 0;
+        for (int i = 0; i < 30; i++)
+            if (arr[i] % 2 != 0 && arr[i] > 80) {
+                sum += arr[i];
+                count++;
+            }
+        if (count > 0)
+            cout << (float)sum / count;
+    }
+
+    return 0;
 }
