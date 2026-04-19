7ba79003f7a115648e258728fbaacbeb52c95b94
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-04-19 22:43:50 +0200

    Refactor decimal to binary conversion logic

--- tasks/arrays/problem022-decimal-to-binary.cpp
+++ tasks/arrays/problem022-decimal-to-binary.cpp
@@ -5,29 +5,21 @@ Description:
 Converts a decimal number into a 20-bit binary representation.
 */
 
-#include <iostream> 
-using namespace std; 
- 
-int main() 
-{ 
-    int X[20] = {0}; 
-    int n; 
- 
-    cin >> n; 
- 
-    int i = 19; 
- 
-    while(n > 0) 
-    { 
-        X[i] = n % 2; 
-        n = n / 2; 
-        i--; 
-    } 
- 
-    for(int i = 0; i < 20; i++) 
-    { 
-        cout << X[i] << " "; 
-    } 
- 
-    return 0; 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X[20] = {0};
+    int n;
+    cin >> n;
+
+    for (int i = 19; n > 0; i--) {
+        X[i] = n % 2;
+        n = n / 2;
+    }
+
+    for (int i = 0; i < 20; i++)
+        cout << X[i] << " ";
+
+    return 0;
 }
