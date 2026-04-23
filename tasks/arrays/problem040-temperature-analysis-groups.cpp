6d2cfcc539ed8691f48c62df11daf2d655a99a80
Mahmoud Ahmed <mahmoud.ahmed65@msa.edu.eg>
2026-04-23 17:41:38 +0200

    Enhance temperature analysis with user choices
    Added functionality to analyze temperature data based on user choice. Choices include finding minimum temperatures, calculating averages, and identifying days above average.

--- tasks/arrays/problem040-temperature-analysis-groups.cpp
+++ tasks/arrays/problem040-temperature-analysis-groups.cpp
@@ -5,52 +5,79 @@ Description:
 Analyzes temperatures in groups of 10 days by finding minimum, 
 average, and days above average.
 */
+#include <iostream>
+using namespace std;
 
-#include <iostream> 
-using namespace std; 
- 
-int main() 
-{ 
-    int X[30]; 
- 
-    for(int i = 0; i < 30; i++) 
-        cin >> X[i]; 
- 
-    for(int i = 0; i < 30; i += 10) 
-    { 
-        int min = X[i]; 
-        int day = i; 
- 
-        for(int j = i; j < i + 10; j++) 
-        { 
-            if(X[j] < min) 
-            { 
-                min = X[j]; 
-                day = j; 
-            } 
-        } 
- 
-        cout << "Min day: " << day + 1 << endl; 
- 
-        int sum = 0; 
- 
-        for(int j = i; j < i + 10; j++) 
-            sum += X[j]; 
- 
-        double avg = (double)sum / 10; 
- 
-        cout << "Average: " << avg << endl; 
- 
-        cout << "Days greater than avg: "; 
- 
-        for(int j = i; j < i + 10; j++) 
-        { 
-            if(X[j] > avg) 
-                cout << j + 1 << " "; 
-        } 
- 
-        cout << endl << "------" << endl; 
-    } 
- 
-    return 0; 
+int main()
+{
+    int X[30], choice;
+    int pos, sum = 0;
+    float ave;
+    int min;
+
+    // قراءة القيم
+    for(int i = 0; i < 30; i++)
+        cin >> X[i];
+
+    cin >> choice;
+
+    // ================== choice 1 ==================
+    if(choice == 1)
+    {
+        for(int i = 0; i < 30; i += 10)
+        {
+            min = X[i];
+
+            for(int j = i; j < i + 10; j++)
+            {
+                if(min > X[j])
+                {
+                    min = X[j];
+                    pos = j;
+                }
+            }
+
+            cout << pos + 1 << endl;
+        }
+    }
+
+    // ================== choice 2 ==================
+    else if(choice == 2)
+    {
+        for(int i = 0; i < 30; i += 10)
+        {
+            sum = 0;
+
+            for(int j = i; j < i + 10; j++)
+                sum += X[j];
+
+            ave = sum / 10.0;
+
+            cout << ave << endl;
+        }
+    }
+
+    // ================== choice 3 ==================
+    else if(choice == 3)
+    {
+        for(int i = 0; i < 30; i += 10)
+        {
+            sum = 0;
+
+            for(int j = i; j < i + 10; j++)
+                sum += X[j];
+
+            ave = sum / 10.0;
+
+            for(int j = i; j < i + 10; j++)
+            {
+                if(X[j] > ave)
+                    cout << j + 1 << " ";
+            }
+
+            cout << endl;
+        }
+    }
+
+    return 0;
 }
